526f6063cfdb972ad195526d096489061da6aa7f
liupangzi <thesedays@126.com>
2017-07-31 23:53:07 +0800

    add links

--- www/views/about-us/index.php
+++ www/views/about-us/index.php
@@ -34,62 +34,17 @@
                         <a href="https://tech.liuchao.me" target="_blank">
                             <i class="wordpress large icon"></i>
                         </a>
-                        <a href="https://www.linkedin.com/in/liumangchao/" target="_blank">
-                            <i class="linkedin large icon"></i>
+                        <a href="https://github.com/liupangzi" target="_blank">
+                            <i class="github large icon"></i>
                         </a>
-                    </span>
-                </div>
-            </div>
-        </div>
-        <div class="column">
-            <div class="ui fluid card">
-                <div class="image">
-                    <img src="https://s.gravatar.com/avatar/cf16f342d42e86cbded5a49974d4e3e0?s=400">
-                </div>
-                <div class="content">
-                    <div class="header">Chao Liu</div>
-                    <div class="meta">
-                        Developer
-                    </div>
-                    <div class="description">
-                        Web / DevOps / Machine Learning
-                    </div>
-                </div>
-                <div class="extra content">
-                    <span class="right floated">Since Dec 21, 2016</span>
-                    <span>
-                        <a href="https://tech.liuchao.me" target="_blank">
-                            <i class="wordpress large icon"></i>
+                        <a href="https://stackoverflow.com/users/849310/holyghost" target="_blank">
+                            <i class="stack overflow large icon"></i>
                         </a>
                         <a href="https://www.linkedin.com/in/liumangchao/" target="_blank">
                             <i class="linkedin large icon"></i>
                         </a>
-                    </span>
-                </div>
-            </div>
-        </div>
-        <div class="column">
-            <div class="ui fluid card">
-                <div class="image">
-                    <img src="https://s.gravatar.com/avatar/cf16f342d42e86cbded5a49974d4e3e0?s=400">
-                </div>
-                <div class="content">
-                    <div class="header">Chao Liu</div>
-                    <div class="meta">
-                        Developer
-                    </div>
-                    <div class="description">
-                        Web / DevOps / Machine Learning
-                    </div>
-                </div>
-                <div class="extra content">
-                    <span class="right floated">Since Dec 21, 2016</span>
-                    <span>
-                        <a href="https://tech.liuchao.me" target="_blank">
-                            <i class="wordpress large icon"></i>
-                        </a>
-                        <a href="https://www.linkedin.com/in/liumangchao/" target="_blank">
-                            <i class="linkedin large icon"></i>
+                        <a href="https://twitter.com/liu_chao" target="_blank">
+                            <i class="twitter large icon"></i>
                         </a>
                     </span>
                 </div>
