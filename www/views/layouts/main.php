b9b0edb9f0be7d8251294edafb1fd06e3ac42621
liuchao21 <liuchao21@58ganji.com>
2017-08-01 10:58:54 +0800

    remove google adsense in <head></head>

--- www/views/layouts/main.php
+++ www/views/layouts/main.php
@@ -30,13 +30,6 @@
             padding: 3em 0em;
         }
     </style>
-    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-    <script>
-        (adsbygoogle = window.adsbygoogle || []).push({
-            google_ad_client: "ca-pub-7808611005897513",
-            enable_page_level_ads: true
-        });
-    </script>
     <?php $this->head() ?>
 </head>
 <body>
