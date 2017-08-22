768dca0730ccf779aa926c25cca524618e0ec553
liuchao21 <liuchao21@58ganji.com>
2017-08-22 14:59:53 +0800

    add sentry monitor

--- environments/dev/common/config/params-local.php
+++ environments/dev/common/config/params-local.php
@@ -1,3 +1,4 @@
 <?php
 return [
+    'sentryDSN' => 'http://f3ed86b6e6bd4c6a8c7f1ebf77d65dff:b616b38dc3cd438fbc4ffd789bed18d4@192.168.216.128:12000/2',
 ];
--- environments/prod/common/config/params-local.php
+++ environments/prod/common/config/params-local.php
@@ -1,3 +1,4 @@
 <?php
 return [
+    'sentryDSN' => 'http://c77c3b7e00cc4ddca06aa999898179b6:67b0e6fbc1474b5286d174f3d2c74c76@45.32.68.199:12000/2',
 ];
