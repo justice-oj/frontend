36bf6866b0939d91577f385dd259917a4cd345ac
liuchao21 <liuchao21@58ganji.com>
2017-08-28 14:42:01 +0800

    add mention notice under discussions

--- common/services/DiscussionService.php
+++ common/services/DiscussionService.php
@@ -39,17 +39,20 @@ class DiscussionService {
     /**
      * @author  liuchao
      * @mail    i@liuchao.me
-     * @param int $problem_id
-     * @param int $user_id
-     * @param string $content
-     * @return bool
+     * @param   int $problem_id
+     * @param   int $user_id
+     * @param   string $content
+     * @return  bool
      * @desc
      */
     public function addDiscussion(int $problem_id, int $user_id, string $content) {
+        $notice_user_list = [];
         $data = json_decode($content);
         foreach ($data->ops as $block) {
             if (preg_match('/^@\w+$/', $block->insert, $match)) {
-                $block->attributes->link = '/profile?name=' . substr($match[0], 1);
+                $username = substr($match[0], 1);
+                $block->attributes->link = '/profile?name=' . $username;
+                $notice_user_list[] = $username;
             }
         }
 
@@ -57,7 +60,32 @@ class DiscussionService {
         $discussion->problem_id = $problem_id;
         $discussion->user_id = $user_id;
         $discussion->content = json_encode($data);
-        return $discussion->save();
+
+        if ($discussion->save()) {
+            $problem = $discussion->problem;
+            $reply_user = User::findOne($user_id);
+            $user_presenter = new UserPresenter();
+
+            foreach (array_diff(array_unique($notice_user_list), [$reply_user->username]) as $username) {
+                $content = <<< NOTICE
+    <div class="item">
+        <img class="ui avatar image" src="{$user_presenter->showAvatar($reply_user->email, 30)}">
+        <div class="content">
+            <a class="header" href="/profile?name={$reply_user->username}">{$reply_user->username}</a>
+            <div class="description">Mentioned you under discussions of problem 
+            <a href="/problem/discussions?problem_id={$discussion->problem_id}#L{$discussion->id}">
+                <b>{$problem->title}</b>
+            </a>
+            </div>
+        </div>
+    </div>
+NOTICE;
+                NotificationService::addNotice($username, $content);
+            }
+            return true;
+        } else {
+            return false;
+        }
     }
 
 
@@ -67,24 +95,25 @@ class DiscussionService {
      * @param   string $username
      * @param   \common\models\Discussion $discussion
      * @param   int $delta
-     * @return bool
+     * @return  bool
      * @desc    update up-votes of discussion
      */
     public function updateDiscussionVotes($username, $discussion, int $delta) {
         $upvote_user = User::findOne(['username' => $username]);
         $notice_user = User::findOne($discussion->user_id);
 
+        // notice user and upvote user cannot be the same
         if ($notice_user->id == $upvote_user->id) {
             return false;
         }
 
-        // notice if this is an up-vote
+        // notice if this is an up-vote but not a down-vote
         if ($delta > 0) {
             $problem = Problem::findOne($discussion->problem_id);
             $user_presenter = new UserPresenter();
             $content = <<< NOTICE
     <div class="item">
-        <img class="ui avatar image" src="{$user_presenter->showAvatar($upvote_user->email, 28)}">
+        <img class="ui avatar image" src="{$user_presenter->showAvatar($upvote_user->email, 30)}">
         <div class="content">
             <a class="header" href="/profile?name={$upvote_user->username}">{$upvote_user->username}</a>
             <div class="description">Up-voted your discussion under problem 
@@ -98,6 +127,7 @@ NOTICE;
             NotificationService::addNotice($notice_user->username, $content);
         }
 
+        //update DB counter
         $discussion->up_vote = $discussion->up_vote + $delta;
         return $discussion->save();
     }
