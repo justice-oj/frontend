382635e961b1a51dfb0594ac713cd8785d873407
liupangzi <thesedays@126.com>
2018-04-13 23:43:12 +0800

    upgrade

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -4,14 +4,14 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=192.168.216.128;dbname=www_justice_plus',
+            'dsn' => 'mysql:host=192.168.18.128;dbname=www_justice_plus',
             'username' => 'root',
             'password' => 'xaiTIVP7kB$oHuJecEooq#YsziVvVAzW',
             'charset' => 'utf8',
         ],
         'rabbitMQ' => [
             'class' => \common\components\queue\BasicRabbitMQProducer::class,
-            'host' => '192.168.216.128',
+            'host' => '192.168.18.128',
             'port' => 5672,
             'user' => 'justice',
             'password' => 'justice',
@@ -21,14 +21,14 @@ return [
         ],
         'redis' => [
             'class' => 'yii\redis\Connection',
-            'hostname' => '192.168.216.128',
+            'hostname' => '192.168.18.128',
             'port' => 6379,
             'database' => 15,
         ],
         'session' => [
             'class' => 'yii\redis\Session',
             'redis' => [
-                'hostname' => '192.168.216.128',
+                'hostname' => '192.168.18.128',
                 'port' => 6379,
                 'database' => 14,
             ]
