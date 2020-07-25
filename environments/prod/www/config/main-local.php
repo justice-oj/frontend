ab0bc6baf66612b00e6be5c5cb38395dd619a346
liuchao.rd <liuchao.rd@bytedance.com>
2020-07-25 21:44:59 +0800

    :volleyball: upgrade to PHP 7.4

--- environments/prod/www/config/main-local.php
+++ environments/prod/www/config/main-local.php
@@ -4,8 +4,7 @@
 return [
     'components' => [
         'request' => [
-            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => 'fDGfVof6drmwV1LqWV6fTWBLKD937N3w',
         ],
     ],
 ];
